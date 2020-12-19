b550693ea465e46f9fbd510561cb218c966c380b
Anne Li <24998524+fibanneacci@users.noreply.github.com>
2020-12-19 01:30:43 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Container, Row } from 'react-grid-system'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import Layout from '../components/layout'
+import SocialMediaCard from '../components/social-media-card.js'
+
+// Infinite scroll built based on code from react-infinite-scroll-component docs, found here: https://codesandbox.io/s/yk7637p62z?file=/src/index.js:215-1101
+
+class IronicNewsfeed extends React.Component {
+    state = {
+        SocialMediaCards: [
+            <SocialMediaCard key={0} />,
+            <SocialMediaCard key={1} />,
+            <SocialMediaCard key={2} />,
+            <SocialMediaCard key={3} />,
+            <SocialMediaCard key={4} />,
+            <SocialMediaCard key={5} />,
+            <SocialMediaCard key={6} />,
+            <SocialMediaCard key={7} />,
+            <SocialMediaCard key={8} />
+        ]
+    }
+
+    generateNextRow = () => {
+        setTimeout(() => {
+            this.setState({
+                SocialMediaCards: this.state.SocialMediaCards.concat([
+                    <SocialMediaCard key={this.state.SocialMediaCards.length} />,
+                    <SocialMediaCard key={this.state.SocialMediaCards.length + 1} />,
+                    <SocialMediaCard key={this.state.SocialMediaCards.length + 2} />
+                ])
+            });
+        }, 1000);
+    };
+
+    render() {
+        return (
+            <Layout>
+                <Container style={{ marginLeft: `-12.5px`, marginRight: `-27.5px` }}>
+                    <InfiniteScroll
+                        className='infinite-scroll'
+                        style={{ display: `none` }}
+                        dataLength={this.state.SocialMediaCards.length}
+                        next={this.generateNextRow}
+                        hasMore={true}
+                        loading={<p>Loading...</p>}
+                    >
+                        <Row>
+                            {this.state.SocialMediaCards}
+                        </Row>
+                    </InfiniteScroll>
+                </Container>
+            </Layout>
+        );
+    }
+}
+
+export default IronicNewsfeed

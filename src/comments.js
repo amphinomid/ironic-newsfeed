cc2c6769483b2b3d55c14458007651383eb609e9
anne <24998524+amphinomid@users.noreply.github.com>
2022-12-24 18:44:15 -0600

    Update comments.js

--- src/comments.js
+++ src/comments.js
@@ -8,8 +8,6 @@ class CommentsPage extends React.Component {
                 <h2>Thanks for visiting this moderately useless site!</h2>
                 <p>
                     This is sort of a joke project. I tried to build an ironic Social Media Newsfeed™, complete with a Like Button™ and Infinite Scroll™. Shitpost / ad titles are generated using a Markov chain.<br /><br />
-                    Feel free to "like" something if you like it! Or if you love it! Or if you're mostly neutral but you dislike it less than you like it!<br /><br />
-                    See also: <a href="https://anli.io/portfolio" target="_blank" rel="noreferrer">my portfolio</a>, where I incorporated an earlier version <s>and where it's also serving a moderately useless purpose</s>.<br /><br />
                     <b>Technologies:</b> React (react-grid-system, react-infinite-scroll-component, react-timeago)<br /><br />
                     <i>Note: because everything is newly generated each time, "likes" disappear upon refresh.</i>
                 </p>
